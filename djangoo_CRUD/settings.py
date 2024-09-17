687a3db5fcd77f112682764ffc3caac8ecf007b2
Eduardo Hernández Hernández <hernandezedu22@gmail.com>
2024-09-17 12:22:50 +0200

    ..

--- djangoo_CRUD/settings.py
+++ djangoo_CRUD/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'taskdb',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
